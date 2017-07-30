582fd19d98e41886e9c3f52c3b092712550eb26c
Oleksandr Kovalchuk <anxolerd@outlook.com>
2017-07-30 03:24:14 +0300

    Implement application skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+extern crate piston;
+extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
+
+use piston::window::WindowSettings;
+use piston::event_loop::{Events, EventLoop, EventSettings};
+use piston::input::RenderEvent;
+use glutin_window::GlutinWindow;
+use opengl_graphics::{OpenGL, GlGraphics};
+
+
 fn main() {
-    println!("Hello, world!");
+    let opengl = OpenGL::V3_2;
+    let settings = WindowSettings::new("Game of Life", [512; 2])
+        .opengl(opengl)
+        .exit_on_esc(true);
+    let mut window: GlutinWindow = settings.build()
+        .expect("Could not create window");
+
+    let mut events = Events::new(EventSettings::new().lazy(false));
+    let mut gl = GlGraphics::new(opengl);
+
+    while let Some(e) = events.next(&mut window) {
+        if let Some(args) = e.render_args() {
+            gl.draw(args.viewport(), |c, g| {
+                use graphics::{clear};
+
+                clear([1.0; 4], g);
+            });
+        }
+    }
 }
