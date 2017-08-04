a5e10c003f5bd9fe2c3cbdd37a38ca20e1fbb0ce
Oleksandr Kovalchuk <anxolerd@outlook.com>
2017-08-04 23:55:55 +0300

    Make code a little bit easier to read
    - Use a typealias Coords instead of [usize; 2]
    - Make neighbours signature consistent with other functions signatures
    - Simplify neighbours method

--- src/gol_world.rs
+++ src/gol_world.rs
@@ -4,6 +4,8 @@ use std::collections::HashSet;
 /// Size of the Game of Life world
 const SIZE: usize = 64;
 
+type Coords = [usize; 2];
+
 /// Stores Game of Life world information
 pub struct World {
     /// Stores the state of the cells.
@@ -13,7 +15,7 @@ pub struct World {
     pub size: usize,
 
     /// cells to check when calculating next generation
-    cells_to_check: HashSet<[usize; 2]>,
+    cells_to_check: HashSet<Coords>,
 }
 
 
@@ -28,25 +30,25 @@ impl World {
     }
 
     /// Set value to cell
-    pub fn set(&mut self, ind: [usize; 2], val: bool) {
+    pub fn set(&mut self, ind: Coords, val: bool) {
         self.cells[ind[0]][ind[1]] = val;
         self.cells_to_check.insert(ind);
-        for &coords in &self.neighbours(ind[0], ind[1]) {
+        for &coords in &self.neighbours(ind) {
             self.cells_to_check.insert(coords);
         }
     }
 
     /// Get value from cell
-    pub fn get(&self, ind: [usize; 2]) -> bool {
+    pub fn get(&self, ind: Coords) -> bool {
         self.cells[ind[0]][ind[1]]
     }
 
     pub fn next_generation(&mut self) {
-        let mut new_values: Vec<([usize; 2], bool)> = Vec::new();
+        let mut new_values: Vec<(Coords, bool)> = Vec::new();
         let mut cells_to_check = self.cells_to_check.clone();
         self.cells_to_check.clear();
         for coords in cells_to_check.drain() {
-            let neighbours = self.neighbours(coords[0], coords[1]);
+            let neighbours = self.neighbours(coords);
             let alive_count = neighbours.iter()
                 .filter(|&ind| self.get(*ind))
                 .count();
@@ -61,23 +63,17 @@ impl World {
             }
         }
         for &(coords, val) in &new_values {
-            self.set([coords[0], coords[1]], val);
+            self.set(coords, val);
         }
     }
 
-    fn neighbours(&self, x:usize, y:usize) -> Vec<[usize; 2]> {
-        [
-            [(SIZE + x - 1) % SIZE, (SIZE + y - 1) % SIZE],
-            [(SIZE + x - 1) % SIZE, y              % SIZE],
-            [(SIZE + x - 1) % SIZE, (y + 1)        % SIZE],
-            [x,                     (SIZE + y - 1) % SIZE],
-            [x,                     (y + 1)        % SIZE],
-            [(x + 1)        % SIZE, (SIZE + y - 1) % SIZE],
-            [(x + 1)        % SIZE, y              % SIZE],
-            [(x + 1)        % SIZE, (y + 1)        % SIZE],
-        ].iter()
-         .filter(|&ind| ind[0] < SIZE && ind[1] < SIZE)
-         .cloned()
-         .collect()
+    fn neighbours(&self, ind: Coords) -> Vec<Coords> {
+        let xs = [(SIZE + ind[0] - 1) % SIZE, ind[0], (ind[0] + 1) % SIZE];
+        let ys = [(SIZE + ind[1] - 1) % SIZE, ind[1], (ind[1] + 1) % SIZE];
+        vec![
+            [xs[0], ys[0]], [xs[1], ys[0]], [xs[2], ys[0]],
+            [xs[0], ys[1]],                 [xs[2], ys[1]],
+            [xs[0], ys[2]], [xs[1], ys[2]], [xs[2], ys[2]],
+        ]
     }
 }
